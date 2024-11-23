4daf1d58a01ec31b196ea6766176df491f7f3319
Ret1ana <laisesc181@gmail.com>
2024-11-23 14:36:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Qual é o resultado de 10 - 6 x 2?',
+    'O resultado de 10 - 6 x 2 é -2'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil??',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'História',
+    'Em que período da pré-história o fogo foi descoberto?',
+    'Paleolítico'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'O que a palavra legend significa em português?',
+    'Legend em português significa LENDA'
+)
